a5e9ff219c17adc963aa4c6d57c3f551e446e569
Daniel <daniel_ye.nz@hotmail.com>
2022-08-16 00:18:29 -0700

    without weather

--- src/App.js
+++ src/App.js
@@ -9,40 +9,59 @@ import Map from "./components/Map/Map";
 const App = () => {
 
     const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([])
+
     const [childClicked, setChildClicked] = useState(null);
 
     const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState({});
 
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
 
+    //useEffect for location (beginning)
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
             setCoordinates({ lat: latitude, lng: longitude });
         })
     }, []);
-    
-    useEffect(() => {
-        setIsLoading(true);
 
-        getPlacesData(bounds.sw, bounds.ne)
-            .then((data) => {
-                //console.log(data);
-                setPlaces(data);
-                setIsLoading(false);
-            })
-    }, [coordinates, bounds]);
+    //useEffect for rating
+    useEffect(() => {
+        const filteredPlaces = places.filter((place) => place.rating > rating);
+
+        setFilteredPlaces(filteredPlaces);
+    }, [rating]);
+    
+    //useEffect for restaurants, hotels, attractions
+    useEffect(() => {
+        if(bounds.sw && bounds.ne){
+            setIsLoading(true);
+    
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data) => {
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    setFilteredPlaces([]);
+                    setIsLoading(false);
+                })
+        }
+    }, [type, bounds]);
 
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header setCoordinates={setCoordinates}/>
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
-                        places={places} 
+                        places={filteredPlaces.length ? filteredPlaces : places} 
                         childClicked={childClicked}
                         isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
                     />
                     
                 </Grid>
@@ -51,7 +70,7 @@ const App = () => {
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
-                        places={places}
+                        places={filteredPlaces.length ? filteredPlaces : places}
                         setChildClicked={setChildClicked}
                     />
                 </Grid>
